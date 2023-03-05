d78501d68de301397ffb48f5dd1d7fd17dc27b8d
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-05 18:03:32 +0600

    create Example002

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.Write("Введите ваше имя: ");
+string inputUserName = Console.ReadLine();
+
+if (myEquals(inputUserName, "Маша"))
+{
+    Console.WriteLine("Это же МАША!");
+}
+else
+{
+    Console.WriteLine("Привет, " + inputUserName);
+}
+
+
+bool myEquals(string obj1, string obj2)
+{
+    if (obj1.ToLower() == obj2.ToLower()) return true;
+    else return false;
+
+}
