71c47490009e6c36daa92d9b30b2613d08d57071
Aleksandr Malakhov <java@malakhov-dev.ru>
2023-03-05 18:24:39 +0600

    create Example003

---
+++ Example003_FriendsAndDog/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+int time = 0;
+int count = 0;
+int friend = 2;
+int distance = 10000;
+int dogSpeed = 5;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+
+while (distance > 10)
+{
+    if (friend == 1)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine("Count - " + count);
